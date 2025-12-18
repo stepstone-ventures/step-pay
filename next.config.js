5bd4b764041da6ce912f598f56f6fdc00fa6266c
Samuel Kwadwo Nuakoh <samuelkwadwonuakoh@Sam-KNs-M3-MBAIR.local>
2025-12-18 17:58:50 +0000

    Fix CSS loading on Netlify - convert next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
